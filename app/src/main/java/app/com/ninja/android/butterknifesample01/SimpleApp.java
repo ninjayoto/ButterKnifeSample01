c7d2454bedf708e861bbdeadfdf2882639c51034
ninjayoto <boon@Ms-MacBook-Pro.local>
2015-12-21 21:12:28 -0700

    Initial commit

---
+++ app/src/main/java/app/com/ninja/android/butterknifesample01/SimpleApp.java
@@ -0,0 +1,12 @@
+package app.com.ninja.android.butterknifesample01;
+
+import android.app.Application;
+
+//import com.example.butterknife.BuildConfig;
+
+public class SimpleApp extends Application {
+  @Override public void onCreate() {
+    super.onCreate();
+//    ButterKnife.setDebug(BuildConfig.DEBUG);
+  }
+}
